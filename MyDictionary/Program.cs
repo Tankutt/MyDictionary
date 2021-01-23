be713856bfebf98972fbd6f9273d8060871b49f9
TANKUT TAŞDELEN <Tankutt@users.noreply.github.com>
2021-01-23 20:18:37 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> books = new MyDictionary<string, string>();
+            books.Add("1","Şeker Portakalı");
+            books.Add("2", "Körlük");
+            books.Add("3", "İncir Kuşları");
+            books.Add("4", "Suç ve Ceza");
+            books.GetList();
+
+        }
+    }
+}
